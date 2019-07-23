fbfe3145ca65580a6d21c40b8c0af009112d8aa1
Mecherok <dmitrymeshcheryakov99@gmail.com>
2019-07-24 01:20:17 +0700

    Project files added

---
+++ BLogic/Models/ShopComputerModel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BLogic.Models {
+    public class ShopComputerModel {
+        Generator Generator = new Generator();
+        Random rnd = new Random();
+        bool IsWorking = false;
+
+        public List<CashDesk> CashDesks { get; set; } = new List<CashDesk>();
+        public List<Cart> Carts { get; set; } = new List<Cart>();
+        public List<Check> Checks { get; set; } = new List<Check>();
+        public List<Sell> Sells { get; set; } = new List<Sell>();
+        public Queue<Seller> Sellers { get; set; } = new Queue<Seller>();
+
+        public ShopComputerModel() {
+            var sellers = Generator.GetNewSellers(20);
+            Generator.GetNewProducts(1000);
+
+            foreach (var seller in sellers) {
+                Sellers.Enqueue(seller);
+            }
+
+            for (int i = 0; i < 3; i++) {
+                CashDesks.Add(new CashDesk(CashDesks.Count, Sellers.Dequeue()));
+            }
+        }
+
+        public void Start() {
+            IsWorking = true;
+            Task.Run(() => CreateCarts(10, 1000));
+
+            var cashDeskTasks = CashDesks.Select(c => new Task(() => CashDeskWork(c, 1000)));
+            foreach (var task in cashDeskTasks) {
+                task.Start();
+            }
+        }
+
+        public void Stop() {
+            IsWorking = false;
+        }
+
+        private void CashDeskWork(CashDesk cashDesk, int sleep) {
+            while (IsWorking) {
+                if (cashDesk.Count > 0) {
+                    cashDesk.Dequeue();
+                    Thread.Sleep(sleep);
+                }
+            }
+        }
+
+        private void CreateCarts(int customerCounts, int sleep) {
+            while (IsWorking) {
+                var customers = Generator.GetNewCustomers(customerCounts);
+
+                foreach (var customer in customers) {
+                    var cart = new Cart(customer);
+
+                    foreach (var product in Generator.GetRandomProducts(10, 30)) {
+                        cart.Add(product);
+                    }
+
+                    var cash = CashDesks[rnd.Next(CashDesks.Count - 1)];
+                    cash.Enqueue(cart);
+                }
+
+                Thread.Sleep(sleep);
+            }
+        }
+    }
+}
---
+++ BLogicTests/Models/CartTests.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BLogic.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLogic.Models.Tests {
+    [TestClass()]
+    public class CartTests {
+        [TestMethod()]
+        public void CartTest() {
+            // Arrange
+
+            var customer = new Customer() {
+                CustomerID = 1,
+                Name = "testUser"
+            };
+
+            var product1 = new Product() {
+                ProductID = 1,
+                Name = "pr1",
+                Price = 100,
+                Count = 20
+            };
+
+            var product2 = new Product() {
+                ProductID = 2,
+                Name = "pr2",
+                Price = 1000,
+                Count = 200
+            };
+
+            var cart = new Cart(customer);
+
+            var expectedResult = new List<Product>() {
+                product1, product1, product2
+            };
+
+            // Act
+
+            cart.Add(product1);
+            cart.Add(product1);
+            cart.Add(product2);
+
+            var cartResult = cart.GetAll();
+
+            // Assert
+
+            Assert.AreEqual(expectedResult.Count, cartResult.Count);
+            for (int i = 0; i < expectedResult.Count; i++) {
+                Assert.AreEqual(expectedResult[i], cartResult[i]);
+            }
+        }
+    }
+}
